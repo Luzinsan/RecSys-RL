71e63bb3058776d2c700a46a6284263cdcc23c0c
luzinsan <luzinsan@mail.ru>
2025-04-19 12:33:45 +0300

    fix: update gitignore, add dataset downloading script

---
+++ datasets/download_datasets.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+curl -L -o ./datasets/ecommerce-behavior-data-from-multi-category-store.zip\
+  https://www.kaggle.com/api/v1/datasets/download/mkechinov/ecommerce-behavior-data-from-multi-category-store
+unzip ./datasets/ecommerce-behavior-data-from-multi-category-store.zip \
+    -d ./datasets/ecommerce-behavior-data-from-multi-category-store/
+
+
+# curl -L -o ./datasets/ecommerce-purchase-history-from-electronics-store.zip\
+#   https://www.kaggle.com/api/v1/datasets/download/mkechinov/ecommerce-purchase-history-from-electronics-store
+# unzip ./datasets/ecommerce-purchase-history-from-electronics-store.zip \
+#     -d ./datasets/ecommerce-purchase-history-from-electronics-store/
+
+
+# curl -L -o ./datasets/ecommerce-events-history-in-cosmetics-shop.zip\
+#   https://www.kaggle.com/api/v1/datasets/download/mkechinov/ecommerce-events-history-in-cosmetics-shop
+# unzip ./datasets/ecommerce-events-history-in-cosmetics-shop.zip \
+#     -d ./datasets/ecommerce-events-history-in-cosmetics-shop/
+
+# curl -L -o ./datasets/ecommerce-purchase-history-from-jewelry-store.zip\
+#   https://www.kaggle.com/api/v1/datasets/download/mkechinov/ecommerce-purchase-history-from-jewelry-store
+# unzip ./datasets/ecommerce-purchase-history-from-jewelry-store.zip \
+#     -d ./datasets/ecommerce-purchase-history-from-jewelry-store/
+
+
+# curl -L -o ./datasets/ecommerce-events-history-in-electronics-store.zip\
+#   https://www.kaggle.com/api/v1/datasets/download/mkechinov/ecommerce-events-history-in-electronics-store
+# unzip ./datasets/ecommerce-events-history-in-electronics-store.zip \
+#     -d ./datasets/ecommerce-events-history-in-electronics-store/
